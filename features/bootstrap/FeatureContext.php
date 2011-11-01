0e76264e2500ad3531f009f8105057713821ca54
Clement Herreman <clement.herreman@gmail.com>
2011-11-01 18:27:27 +0100

    Adding mink boostrap

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,25 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+require_once realpath(__DIR__) . '/../../vendor/mink/autoload.php.dist';
+
+/**
+* Features context.
+*/
+class FeatureContext extends Behat\Mink\Behat\Context\MinkContext
+{
+    /**
+* @Then /^I wait for the suggestion box to appear$/
+*/
+    public function iWaitForTheSuggestionBoxToAppear()
+    {
+        $this->getSession()->wait(5000, "$('.suggestions-results').children().length > 0");
+    }
+}
+
