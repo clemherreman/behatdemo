0d279711abfa222b3f7202ab095f4ab9e88ecc83
Clement Herreman <clement.herreman@gmail.com>
2011-11-01 17:52:59 +0100

    Changed folder organisation + dummy website now calculate a square parameter

---
+++ website/index.php
@@ -0,0 +1,39 @@
+<?php
+
+//phpinfo();
+require_once __DIR__.'/silex.phar'; 
+
+$app = new Silex\Application();
+$app['debug'] = true; 
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path'       => __DIR__.'/views',
+    'twig.class_path' => __DIR__.'/vendor/twig/lib',
+));
+
+$app->get('/square', function() use($app) { 
+    return $app['twig']->render('square.twig', array(
+        'errors'    => false,
+        'perimeter' => false,
+        'side'      => false,
+    ));
+});
+
+$app->post('/square', function() use($app) {
+  $side = $app['request']->get('side');
+  
+  if (!is_numeric($side))
+  {
+    $errors[] = 'Please input a number as side size';
+  }
+  
+  $perimeter = empty($errors) ? $side*$side : null;
+  
+  return $app['twig']->render('square.twig', array(
+    'errors'    => $errors,
+    'perimeter' => $perimeter,
+    'side'      => $side,
+  ));
+});
+
+$app->run(); 
